86f59d95e4f8e50a027522c2fd6e908f7404ddaa
its001 <itspace.001@gmail.com>
2022-06-16 17:56:05 +0300

    Upd init

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Fomvasss\Seo\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
---
+++ src/Facades/Seo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fomvasss\Seo\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Fomvasss\Seo\Seo
+ */
+class Seo extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-seo';
+    }
+}
